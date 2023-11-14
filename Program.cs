853fe260a43bf743be95ed19ccce6c273f79b919
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-11-14 20:37:09 +0300

    Текстовое описание решения

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}] ");
     }
     Console.WriteLine();
 }
